7d62cf9dd866ef4f722f8f2f98e437c360e8ae04
Ethan Seguin <ethan.seguin@techhigh.us>
2015-08-25 21:37:16 -0400

    Apples

---
+++ apples.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+class apples {
+public static void main (String args[])
+{
+Scanner bucky = new Scanner(System.in)	;
+
+int tuna = 5;
+int bass = 18;
+
+tuna +=6;
+
+System.out.println(tuna);
+
+}
+}
